1896318aa538ba7a7126767ecfbbb76fcb5d0309
unknown <asim981201@gmail.com>
2025-02-19 18:00:05 +0545

    new attendance

--- lib/src/features/attendance/presentation/ui/attendance.dart
+++ lib/src/features/attendance/presentation/ui/attendance.dart
@@ -4,6 +4,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:hrm/src/core/resources/color_manager.dart';
 import 'package:hrm/src/core/utils/router.dart';
 import 'package:hrm/src/core/widgets/common_widgets.dart';
 import 'package:hrm/src/features/attendance/data/sample_data/monthly_data.dart';
@@ -122,65 +123,61 @@ class Attendance extends StatelessWidget {
               ...List.generate(sampleAttendanceList.length, (index){
                 final attendanceData = sampleAttendanceList[index];
                 final date = DateFormat('yyyy-MM-dd').parse(attendanceData['startDate']);
-                final month = DateFormat('MMMM yyyy').format(date);
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(month,style: bh2,),
-                    h10,
-                    Card(
-                      margin: EdgeInsets.only(bottom: 20),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: InkWell(
-                          onTap: ()=>routeTo(context, AttendanceSearch()),
-                          child: SizedBox(
-                            width: double.infinity,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text('Total Days', style: br2,maxLines: 1,),
-                                    Text('${attendanceData['totalDays']}', style: bh2,),
-                                  ],
-                                ),
-                                VertDivider(),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text('Attended', style: br2,maxLines: 1,),
-                                    Text('${attendanceData['attendedDays']}', style: bh2,),
-                                  ],
-                                ),
-                                VertDivider(),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text('Leave', style: br2,maxLines: 1,),
-                                    Text('${attendanceData['leave']}', style: bh2,),
-                                  ],
-                                ),
-                                VertDivider(),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text('Total %', style: br2,maxLines: 1,),
-                                    Text('${attendanceData['aggrv']}%', style: bh2,),
-                                  ],
-                                ),
-                              ],
-                            ),
+                final month = DateFormat('MMM').format(date);
+                final year = DateFormat('yyyy').format(date);
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: Material(
+                    color: MyColors.white,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.only(left: 16),
+                      tileColor: MyColors.lightGrey,
+                      shape: ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)
+                      ),
+                      onTap: ()=>routeTo(context, AttendanceSearch()),
+                      leading: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(month,style: bh2),
+                          Text(year,style: br2,)
+                        ],
+                      ),
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          VertDivider(),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text('Total Days', style: br2,),
+                              Text('${attendanceData['totalDays']}', style: bh2,),
+                            ],
                           ),
-                        ),
+                          VertDivider(),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Attended', style: br2,maxLines: 1,),
+                              Text('${attendanceData['attendedDays']}', style: bh2,),
+                            ],
+                          ),
+                          VertDivider(),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Leave', style: br2,maxLines: 1,),
+                              Text('${attendanceData['leave']}', style: bh2,),
+                            ],
+                          ),
+                          Icon(Icons.chevron_right_rounded),
+                        ],
                       ),
                     ),
-                  ],
+                  ),
                 );
               })
             ],
--- lib/src/features/utilities/data/utilities_list.dart
+++ lib/src/features/utilities/data/utilities_list.dart
@@ -1,16 +1,11 @@
 
 
 
-
-import 'package:hrm/src/features/attendance/presentation/ui/attendance.dart';
-
-import '../../attendance/presentation/ui/attendance_search.dart';
+import '../../attendance/presentation/ui/attendance.dart';
 import '../../calendar/presentation/ui/calendar.dart';
 import '../../departments/presentation/ui/all_departments.dart';
 
 final utilitiesList = [
-
-  //index should match index in the list of dashboardItems (D:\hrm\lib\src\features\dashboard\data\sample_data\sample_menu_list.dart)
 
   {'name': 'Field Visit', 'image': 'assets/images/field_visit.png', 'route' : null},
   {'name': 'Leave', 'image': 'assets/images/leave.png', 'route' : null},
